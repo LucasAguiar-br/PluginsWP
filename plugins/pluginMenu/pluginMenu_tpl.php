8219a7c09a01b5033e284e1048fdcc2095a675cd
Lucas Silva <lucas.s-aguiar@hotmail.com>
2020-11-03 20:50:51 -0300

    aula do dia 0311 com plug in de menu

---
+++ plugins/pluginMenu/pluginMenu_tpl.php
@@ -0,0 +1,13 @@
+<div class="wrap">
+    <h1> Configurações do Meu Plug-in</h1>
+    <form method="post" action="option.php">
+
+        <label for="">Token da API</label>
+        <input type="text" id="token_da_api" name="token_da_api">
+        <br><br>
+        <label for="">Token da API</label>
+        <input type="text" id= "url_da_api" name="url_da_api">
+
+        <?php submit_button(); ?>
+    </form>
+</div>
---
+++ plugins/pluginMenu/pluginmenu.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+*Plugin Name: Plugin Menu
+* Plugin URI: https://sp.senac.br
+* Description: Este é um plugin que mostra como trabalha com menu do admin do wordpress
+* Version: 0.0.1
+* Author: Lucas Silva
+* Author URI: https://sp.senac.br
+* Lincese: CC BY
+*/
+
+
+
+add_action( 'admin_menu', 'menu_plugin');
+
+
+function menu_plugin() {
+    //criar nível item no primeiro nível do menu
+
+  /*  
+    add_menu_page( 'Configurações do meu plugin',
+                    'Meu plug-in',
+                    'administrator',
+                    'meu-plugin-config',
+                    'abre_configuracoes',
+                    'dashicons-admin-generic');
+
+*/
+
+    add_submenu_page (  'options-general.php',
+                        'configurações do meu Plug-in',
+                        'Meu Plug-in',
+                        'administrator',
+                        'meu-plugin-config',
+                        'abre_configuracoes');
+}
+
+function abre_configuracoes(){
+
+    require 'pluginMenu_tpl.php';
+
+}
